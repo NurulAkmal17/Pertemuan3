05aeebe04b4dbb3983f0bcff16e27c859996dda4
Nurul Akmal <nakmal618@gmail.com>
2022-03-24 15:11:43 +0700

    kode program Person untuk lebih ringkasnya lagi

---
+++ src/main/kotlin/5_PersonC.kt
@@ -0,0 +1,7 @@
+//Untuk lebih ringkasnya lagi, deklarasi dan inisialisasi property dapat dilakukan hanya didalam parameter konstruktor utama
+class PersonC (var firstName: String, var lastName: String, var age: Int) {}
+fun main(){
+    val budi = PersonC("Budi", "Gunawan", 21)
+    println("Name : ${budi.firstName} ${budi.lastName}")
+    println("Age : ${budi.age}")
+}
