fd2ea179b39718289be2607b93bdf721fad3888e
Nurul Akmal <nakmal618@gmail.com>
2022-03-24 12:20:29 +0700

    contoh kode program Initializer order

---
+++ src/main/kotlin/2_InitOrder.kt
@@ -0,0 +1,15 @@
+//Blok inisialisasi diawali dengan keyword init. Selama instansiasi sebuah objek, urutan eksekusi blok inisialisasi sama dengan urutan yang ada pada tubuh kelas, berselangkan inisialisasi property-nya.
+//Parameter dari konstruktor utama dapat digunakan dalam blok inisialisasi. Ia juga dapat digunakan untuk menginisialisasi property yang dideklarasikan dalam tubuh kelas.
+class InitOrder(name: String) {
+    val firstProperty = "First property: $name".also(::println)
+    init {
+        println("First initializer block that prints ${name}")
+    }
+    val secondProperty = "Second property: ${name.length}".also(::println)
+    init {
+        println("Second initializer block that prints ${name.length}")
+    }
+}
+fun main(){
+    val initOrder = InitOrder("RPL")
+}
